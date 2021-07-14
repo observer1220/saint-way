c2221676d52cfadb10b38673a6cb80d7a9c4da99
KentShen <observer1220@gmail.com>
2021-07-14 12:38:39 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ npm run build
 git add -A
 git commit -m 'deploy'
 # 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
-git push -f git@github.com/observer1220/saint-way.git master:gh-pages
+git push -f git@github.com:observer1220/saint-way.git master:gh-pages
 //將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
 cd -

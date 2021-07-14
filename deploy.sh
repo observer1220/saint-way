c418aa4324c93b3b8f7ddbad9c44158b383f0d35
KentShen <observer1220@gmail.com>
2021-07-14 12:36:39 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,11 @@ set -e
 # 打包
 npm run build
 # 移動至到打包後的dist目錄 
-cd docs/dist
-git init //因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
+# cd dist
+# git init //因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
 git add -A
 git commit -m 'deploy'
 # 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
-git push origin main
+git push -f git@github.com/observer1220/saint-way.git master:gh-pages
 //將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
 cd -
